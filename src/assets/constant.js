12eb090fcbb58cd29782097767ba5c2766cebc89
Sai Than Tun Sein <saisaingwann188@gmail.com>
2024-03-26 20:34:23 +0630

    add new project

--- src/assets/constant.js
+++ src/assets/constant.js
@@ -1,3 +1,4 @@
 export {default as keepontack} from './keepontrack.png'
 export {default as passwordGenerator} from './passwordGenerator.png'
 export {default as PersonalPortfolio} from './PersonalPortfolio1.png'
+export {default as BookShopByDay} from './bookshopbyday.png'
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
-import { PersonalPortfolio, keepontack, passwordGenerator } from "../assets/constant";
+import { BookShopByDay, PersonalPortfolio, keepontack, passwordGenerator } from "../assets/constant";
 
 export const projects = [
     {name: 'KeepOnTrack',
@@ -11,6 +11,11 @@ img: PersonalPortfolio,
 descripiton: "In my portfolio website, I've invested considerable effort into optimizing the user experience by prioritizing simplicity, cleanliness, and functionality. Ensuring responsiveness across all screen sizes is a top priority, while I've also integrated Framer Motion to introduce smooth and captivating animations.",
 siteUrl: 'https://daydev.netlify.app/',
 techStats:  [ 'React', 'Redux Toolkit', "Tailwind","Framer Motion"]},
+    {name: 'BookShopByDay',
+img: BookShopByDay,
+descripiton: "This one stands as a testament to my passion for frontend development and my dedication to creating engaging digital experiences. By combining creativity, technical expertise, and attention to detail, I've crafted a website demonstrates my skills as a frontend developer.In this one mostly working with API integration ,JS for user interactivity, resposive layout for all screen and routing with react router dom.",
+siteUrl: 'https://bookshopbyday.netlify.app/',
+techStats: ['React',"Tailwind","React Router Dom"]},
     {name: 'Password Generator',
 img: passwordGenerator,
 descripiton: "The Password Generator offers a range of customization options, allowing users to specify the length of the password, include various character types (such as uppercase letters, lowercase letters, numbers, and special characters).Upon selecting their preferences, users can generate a unique password with a single click. The frontend of the application was built using HTML, TailWind Css, and TypeScript to create an intuitive,user-friendly and responsive user interface.Implementing the password generation logic and handling user interactions with TypeScript.",
